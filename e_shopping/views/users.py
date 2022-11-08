5a9ca21bd365a2f6c14e2f038da45b7e27341448
benard42 <byakatondabenard13@gmail.com>
2022-11-08 15:23:55 +0300

    modified

--- e_shopping/views/users.py
+++ e_shopping/views/users.py
@@ -2,15 +2,18 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from e_shopping.models import User
 from flask_login import login_user, logout_user, login_required, current_user
 
-userviews = Blueprint('userviews', __name__, url_prefix = '/users')
+userviews = Blueprint('userviews', __name__, url_prefix='/users')
+
+
 
 @userviews.route('/')
 def index():
     if not current_user.is_authenticated:
         return redirect(url_for('userviews.login'))
-    return render_template('home.html', user = current_user)
+    return render_template('home.html', user=current_user)
 
-@userviews.route('/login', methods = ['GET', 'POST'])
+
+@userviews.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('userviews.index'))
@@ -20,16 +23,16 @@ def login():
 
         user = User.get_user_by_username(username)
 
-        if user and User.verify_password(password,user.password):
-            login_user(user,remember = True)
+        if user and User.verify_password(password, user.password):
+            login_user(user, remember=True)
             return redirect(url_for('userviews.index'))
         else:
             flash('Login failed')
             return redirect(url_for('userviews.login'))
-        return render_template('login.html')
+    return render_template('login.html')
 
 
-@userviews.route('/register',methods = ['GET', 'POST'])
+@userviews.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('userviews.index'))
@@ -38,8 +41,7 @@ def register():
         email = request.form.get('email')
         password = request.form.get('password')
 
-        #check if user already exists
-
+        # check if user already exists
         existing_user = User.get_user_by_email(email)
         if existing_user:
             flash('Email already exists')
@@ -49,24 +51,25 @@ def register():
             flash('Username already exists')
             return redirect(url_for('userviews.register'))
 
-        #create new user
+        # create new user
         user = User(
             username=username,
-            email= email,
+            email=email,
             password=User.hash_password(password)
         )
         user.save()
-        flash('your account has been created, please login')
+        flash('Your account has been created, please login')
         return redirect(url_for('userviews.login'))
     return render_template('register.html')
 
 
-@userviews.route('logout', methods = ['POST','GET'])
+
+@userviews.route('/logout', methods=['POST', 'GET'])
 @login_required
 def logout():
     if request.method == 'POST':
         logout_user()
         return redirect(url_for('userviews.login'))
-
+    
     logout_user()
-    return redirect(ur('userviews.login'))
+    return redirect(url_for('userviews.login'))
